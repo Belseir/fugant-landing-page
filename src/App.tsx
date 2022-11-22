d48925bc40d5dc72202fa8bd94c4916242db4eff
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-21 23:53:28 -0300

    refactor: Remove scrolLToTop and add scroll pivot

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,26 @@ import ProductSection from "./components/ProductSection";
 import useIsInViewport from "./hooks/useIsInViewport";
 import { useRef } from "react";
 import ScrollToTopButton from "./components/ScrollToTopButton";
+import ContactSection from "./components/ContactSection";
 
 export default function App() {
   const headingRef = useRef() as React.MutableRefObject<HTMLHeadingElement>;
+  const topRef = useRef() as React.MutableRefObject<HTMLDivElement>;
   const { visible: headingVisible } = useIsInViewport(headingRef);
-
-  const scrollToTop = () => {
-    headingRef.current.scrollIntoView({
-      behavior: "smooth",
-    });
-  };
 
   return (
     <div className="font-poppins w-screen h-screen overflow-scroll overflow-x-hidden">
+      <div
+        ref={topRef}
+        className="relative top-0 left-0 w-full h-[1px] opacity-0"
+      >
+        Scroll Pivot - IGNORE
+      </div>
       <Logo opacity={headingVisible ? undefined : 0.1} />
       <MainSection ref={headingRef} />
       <ProductSection />
-      <ScrollToTopButton enabled={!headingVisible} onClick={scrollToTop} />
+      <ContactSection />
+      <ScrollToTopButton enabled={!headingVisible} topRef={topRef} />
     </div>
   );
 }
