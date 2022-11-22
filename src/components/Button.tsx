eee23dc92b3944111e33686aaba0925ac04e597b
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-21 23:53:08 -0300

    feat: Add className prop and twMerge

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,21 @@
+import { twMerge } from "tailwind-merge";
+
 const Button = ({
   content,
+
+  className,
   onClick,
 }: {
   content: string;
+  className?: string;
   onClick?: () => void;
 }) => {
   return (
     <button
-      className="py-3 px-10 text-xl text-white flex flex-wrap items-center  rounded-lg border-2 border-[#19608A] bg-[#2FA9F0] text-secondary shadow-[0_0.2rem_00#19608A] outline-none transition-all  hover:-translate-y-1 hover:shadow-[0_0.4rem_00#19608A] active:translate-y-1 active:shadow-[0_0rem_00#19608A]"
+      className={twMerge(
+        `py-3 px-10 text-xl text-white justify-center flex flex-wrap items-center rounded-lg border-2 border-[#19608A] bg-[#2FA9F0] text-secondary shadow-[0_0.2rem_00#19608A] outline-none transition-all  hover:-translate-y-1 hover:shadow-[0_0.4rem_00#19608A] active:translate-y-1 active:shadow-[0_0rem_00#19608A]`,
+        className
+      )}
       onClick={onClick}
     >
       {content}
