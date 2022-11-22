94ff4bca7dba011b35ca9b380dbbd8d77675608a
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-21 22:15:21 -0300

    feat: Add scrollToTop function

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,24 @@ import MainSection from "./components/MainSection";
 import ProductSection from "./components/ProductSection";
 import useIsInViewport from "./hooks/useIsInViewport";
 import { useRef } from "react";
+import ScrollToTopButton from "./components/ScrollToTopButton";
 
 export default function App() {
   const headingRef = useRef() as React.MutableRefObject<HTMLHeadingElement>;
-  const { visible } = useIsInViewport(headingRef);
+  const { visible: headingVisible } = useIsInViewport(headingRef);
+
+  const scrollToTop = () => {
+    headingRef.current.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
 
   return (
     <div className="font-poppins snap-y w-screen h-screen snap-proximity overflow-scroll overflow-x-hidden">
-      <Logo opacity={visible ? undefined : 0.1} />
+      <Logo opacity={headingVisible ? undefined : 0.1} />
       <MainSection ref={headingRef} />
       <ProductSection />
+      <ScrollToTopButton enabled={!headingVisible} onClick={scrollToTop} />
     </div>
   );
 }
