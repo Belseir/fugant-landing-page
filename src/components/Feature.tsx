c2b8b0d3610eafc09dfe1c6a3574198bd22d0162
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-21 22:47:28 -0300

    refactor: Pass text as children

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,14 +1,18 @@
 import { Icon } from "@iconify/react";
 
-const Feature = ({ text, icon }: { text: string; icon: string }) => {
+interface FeatureProps extends React.PropsWithChildren {
+  icon: string;
+}
+
+const Feature: React.FunctionComponent<FeatureProps> = ({ icon, children }) => {
   return (
     <div className="flex flex-row items-center gap-5">
       <Icon
-        className="bg-[#102229] text-[#2C5364] drop-shadow-lg rounded w-12 h-12 md:w-16 md:h-16"
+        className="bg-[#102229] text-[#2C5364] drop-shadow-lg rounded p-2 w-12 h-12 md:w-16 md:h-16"
         icon={icon}
       />
       <p className="text-[#D9D9D9] font-bold text-xl md:text-2xl drop-shadow-lg">
-        {text}
+        {children}
       </p>
     </div>
   );
